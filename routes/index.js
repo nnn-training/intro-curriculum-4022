4f62ceec6b7ef1c194a5244253e5cbd2395b8f04
ya <motivator@seiunsky.sakura.ne.jp>
2021-08-17 09:09:30 +0900

    日本標準時に変更しYYYY/MM/DD HH:mm形式で表示

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ const express = require('express');
 const router = express.Router();
 const Schedule = require('../models/schedule');
 
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
   const title = '予定調整くん';
@@ -13,10 +19,13 @@ router.get('/', (req, res, next) => {
       },
       order: [['updatedAt', 'DESC']]
     }).then((schedules) => {
+      schedules.forEach((schedule) => {
+        schedule.formattedUpdatedAt = dayjs(schedule.updatedAt).tz('Asia/Tokyo').format('YYYY/MM/DD HH:mm');
+      });
       res.render('index', {
         title: title,
         user: req.user,
-        schedules: schedules
+        schedules: schedule.formattedUpdatedAt
       });
     });
   } else {
